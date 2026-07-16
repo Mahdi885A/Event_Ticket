7b07cc54b7a025e3767036a7f9325afdf760262c
Mahdi <mahdiazimi.885@gmail.com>
2026-07-16 22:50:55 +0330

    Add GenericService

---
+++ service/GenericService.java
@@ -0,0 +1,11 @@
+package service;
+
+import java.util.List;
+
+public interface GenericService<T> {
+    Long register(T t);
+    T change(T t);
+    Long cancel(Long id);
+    T getById(Long id);
+    List<T> getAll();
+}
